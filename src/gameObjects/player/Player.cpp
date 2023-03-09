d767f0bf8193ad572475ffac8d70cae146327e06
Ricardo Palomares <rpmdisguise-java@yahoo.es>
2023-03-10 00:38:24 +0100

    Player sources free of syntax errors

--- src/gameObjects/player/Player.cpp
+++ src/gameObjects/player/Player.cpp
@@ -1,16 +1,35 @@
 #include "Player.h"
+#include "ofMain.h"
 
 
-// pmInit Function
+// Constructor con tipos simples
 //*************************************************************
-void Player::init(int nInitialPosX, int nInitialPosY){
+Player::Player(int x, int y, int canyonAngle, int r, int g, int b) {
+	this->bodyPos.x = x;
+	this->bodyPos.y = y;
+    this->canyonAngle = canyonAngle;
+    this->mainColor = ofColor(r % 256, g % 256, b % 256);
+}
 
-	this->initialPosX = nInitialPosX;
-	this->initialPosY = nInitialPosY;
-	this->posX = nInitialPosX;
-	this->posY = nInitialPosY;
+// Constructor con tipos reales
+//*************************************************************
+Player::Player(ofPoint playerPos, int canyonAngle, ofColor mainColor) {
+	this->bodyPos = playerPos;
+    this->canyonAngle = canyonAngle;
+    this->mainColor = mainColor;
+}
 
-    return;
+// pmDraw Function
+//*************************************************************
+void Player::draw(){
+    // TODO: incluir en una matriz de transformación
+	ofSetColor(this->mainColor);
+    // TODO: hacer un degradado de color;
+	ofFill();
+    ofDrawCircle(bodyPos, radius);
+    // TODO: incluir en una matriz de transformación
+	ofDrawRectangle(0, 0 - this->canyonWidth / 2, this->canyonLength, this->canyonWidth / 2);
+	return;
 }
 
 
@@ -23,21 +42,10 @@ void Player::update(){
 }
 
 
-// pmDraw Function
-//*************************************************************
-void Player::draw(){
-	
-	ofSetColor(200, 200, 200);
-	ofFill();
-	ofDrawRectangle(this->posX, this->posY, 100, 100);
-	return;
-}
-
-
 // MoveLeft Function
 //*************************************************************
 void Player::moveLeft(){
-    this->posX = this->posX - this->speed;
+    this->bodyPos.x = this->bodyPos.x - this->rotationSpeed;
 	return;
 }
 
@@ -45,6 +53,6 @@ void Player::moveLeft(){
 // MoveRight Function
 //*************************************************************
 void Player::moveRight(){
-	this->posX = this->posX + this->speed;
+	this->bodyPos.x = this->bodyPos.x + this->rotationSpeed;
 	return;
 };
--- src/gameObjects/player/Player.h
+++ src/gameObjects/player/Player.h
@@ -3,20 +3,19 @@
 
 class Player {
 	public:
-
-		const int color1_R = 253, color1_G = 255, color1_B = 0;
-		const int color2_R = 0, color2_G = 0, color2_B = 0;
-		const int speed = 5;
-		const int speedMouse = 5;
+		const int rotationSpeed = 2;
 		const int radius = 80;
-		const int radiusEye = radius/10;
-		const int maxAngleMouse = 60;
+        const int canyonLength = 50;
+        const int canyonWidth = 8;
 
+        ofColor mainColor;
+        ofColor altColor;
+        ofPoint bodyPos;
+        ofPoint canyonEdgePos;
+        int canyonAngle;
 
-		int initialPosX, initialPosY;
-		int posX, posY;
-
-		void init(int nInitialCenterX, int nInitialCenterY);
+        Player(int x, int y, int canyonAngle, int r, int g, int b);
+		Player(ofPoint playerPos, int canyonAngle, ofColor = ofColor::blue);
 		void draw();
 		void update();
 		void moveLeft();
