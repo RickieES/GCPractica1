459d12ace51497c9a89c417af643c8ed70d5b153
Ricardo Palomares <rpmdisguise-java@yahoo.es>
2023-03-20 14:36:55 +0100

    Bullet class created, WIP

---
+++ src/gameObjects/bullet/Bullet.cpp
@@ -0,0 +1,26 @@
+#include "Bullet.h"
+#include "ofMain.h"
+
+
+void Bullet::setSpeed(int s) {
+    this->speed = s;
+}
+
+int Bullet::getSpeed() {
+    return this->speed;
+};
+
+// pmDraw Function
+//*************************************************************
+void Player::draw() {
+	ofPushMatrix();
+	ofTranslate(this->getRefPointX(), this->getRefPointY(), 0);
+
+	ofSetColor(this->getColor());
+	ofFill();
+    // Please keep in mind that drawing circle with different outline color and fill
+    // requires calling ofNoFill and ofSetColor for drawing stroke and ofFill and
+    // again ofSetColor for filled solid color circle.
+    ofDrawCircle(0, 0 , 10);
+	ofPopMatrix();
+}
---
+++ src/gameObjects/bullet/Bullet.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ofMain.h"
+#include "../gameObject.h"
+
+class Bullet: public GameObject {
+    private:
+        int speed;
+    
+    public:
+        void setSpeed(int s);
+        int getSpeed();
+};
